15764d1f7c1652576b26cc4d64f8e318f1edf226
Dan Narsavage <dannarsavage@users.noreply.github.com>
2022-02-21 09:35:47 -0700

    Altered App to show a quote. Next step: hit API.

--- src/App.js
+++ src/App.js
@@ -5,12 +5,30 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <QuoteFromMySon
+          AgeWhenUttered="65"
+          QuoteText="This is hard coded"
+          BeforeAction={null}
+          AfterAction={null}
+        />
       </header>
     </div>
   );
+}
+
+function QuoteFromMySon(props) {
+  const ageWhenUttered = props.AgeWhenUttered;
+  const quoteText = props.QuoteText;
+  const beforeAction = props.BeforeAction;
+  const afterAction = props.AfterAction;
+  return (
+    <div className="ThingMySonSays">
+      <p>Age {ageWhenUttered}</p>
+      <p>{beforeAction}</p>
+      <p>{quoteText}</p>
+      <p>{afterAction}</p>
+    </div>
+  )
 }
 
 export default App;
